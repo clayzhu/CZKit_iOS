49a6eec0dbeb54bf2323a9f2f5929dca63f59e2d
Clay Zhu <clayzhu@163.com>
2017-03-21 16:36:53 +0800

    创建 CZLaunchVC Demo VC.

---
+++ CZKitDemo/CZKitDemo/DemoVC/CZLaunchVCDemoVC/CZLaunchVCDemoVC.h
@@ -0,0 +1,13 @@
+//
+//  CZLaunchVCDemoVC.h
+//  CZKitDemo
+//
+//  Created by Apple on 2017/3/21.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZLaunchVCDemoVC : UIViewController
+
+@end
---
+++ CZKitDemo/CZKitDemo/DemoVC/CZLaunchVCDemoVC/CZLaunchVCDemoVC.m
@@ -0,0 +1,38 @@
+//
+//  CZLaunchVCDemoVC.m
+//  CZKitDemo
+//
+//  Created by Apple on 2017/3/21.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import "CZLaunchVCDemoVC.h"
+
+@interface CZLaunchVCDemoVC ()
+
+@end
+
+@implementation CZLaunchVCDemoVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+	self.title = @"多种形式启动页";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

698b76757305d18831b9b98033120ff01de91029
Clay Zhu <clayzhu@163.com>
2017-06-10 23:37:17 +0800

    添加 CZInspectableViewDemoVC。

---
+++ CZKitDemo/CZKitDemo/DemoVC/CZInspectableViewDemoVC/CZInspectableViewDemoVC.h
@@ -0,0 +1,13 @@
+//
+//  CZInspectableViewDemoVC.h
+//  CZKitDemo
+//
+//  Created by ug19 on 2017/6/10.
+//  Copyright © 2017年 Clay Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZInspectableViewDemoVC : UIViewController
+
+@end
